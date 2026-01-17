4bd8f04e743bea79394b6cb9fbb326b7b81dad1a
Parmita <160781867+kabbo1031@users.noreply.github.com>
2026-01-17 19:28:45 +0600

    Create abi.js

---
+++ abi.js
@@ -0,0 +1,6 @@
+const TOKEN_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function decimals() view returns (uint8)",
+    "function balanceOf(address owner) view returns (uint256)"
+];
